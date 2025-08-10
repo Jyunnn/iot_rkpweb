04f647e41f789e0b5e70aa7fdb4462a751e5286e
Jyun Wei <43448066+Jyunnn@users.noreply.github.com>
2025-08-10 10:59:14 +0800

    refactor: use scss style blocks

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -71,12 +71,14 @@
   ]
 </script>
 
-<style scoped lang="sass">
-  .social-link :deep(.v-icon)
-    color: rgba(var(--v-theme-on-background), var(--v-disabled-opacity))
-    text-decoration: none
-    transition: .2s ease-in-out
+<style scoped lang="scss">
+  .social-link :deep(.v-icon) {
+    color: rgba(var(--v-theme-on-background), var(--v-disabled-opacity));
+    text-decoration: none;
+    transition: .2s ease-in-out;
 
-    &:hover
-      color: rgba(25, 118, 210, 1)
+    &:hover {
+      color: rgba(25, 118, 210, 1);
+    }
+  }
 </style>
